aed02e5aa6fec65b5223d3c1d57846605989228e
Isaac-onah <iumeha@yahoo.com>
2025-03-04 10:31:10 +0100

    navigation bar

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,38 @@
-import React from "react"
+import Link from "next/link";
+import {Button} from "./ui/button";
+
+import Nav from "./Nav";
 
 const Header = () => {
-return <div>Header</div>;
+return (
+    <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto">
+            {/* logo*/}
+            <Link href="/">
+                <h1 className="text-4xl font-semibold">
+                    Isaac <span className="text-accent">.</span>
+                </h1>
+            </Link>
+
+             {/* Desktop navigation and hire me button*/}
+             <div className="hidden xl:flex items-center gap-8">
+                <Nav />
+                <Link href="/contact">
+                        <Button>Hire Me</Button>
+                </Link>
+             </div>
+
+
+             {/* mobile navigation and hire me button*/}
+             <div className="xl:hidden">
+                <Nav />
+                <Link href="/contact">
+                        <Button>Hire Me</Button>
+                </Link>
+             </div>
+        </div>
+    </header>
+);
 }
 
 export default Header
