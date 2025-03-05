90b91e276913c62e4eebcd6d4d418fdc16471e62
Isaac-onah <iumeha@yahoo.com>
2025-03-05 12:56:52 +0100

    transition stairs

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,9 @@
 import {JetBrains_Mono} from "next/font/google";
 import "./globals.css";
+
 import Header from "@/components/Header";
+import PageTransition from "@/components/PageTransition";
+import StairTransition from "@/components/StairTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets:["latin"],
@@ -17,10 +20,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={jetbrainsMono.variable}
-      >
-        <Header/>
-        {children}
+        className={jetbrainsMono.variable}>
+        <Header />
+        <StairTransition />
+        <PageTransition>{children}</PageTransition>
       </body>
     </html>
   );
